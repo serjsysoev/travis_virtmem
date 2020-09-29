16122011ec97b61ee7a39edefdf2391bc20a07c6
Sergey Sysoev <sergey.sysoev02@gmail.com>
2020-09-29 11:42:59 +0300

    Added test generation

--- src/ArgumentsParser.kt
+++ src/ArgumentsParser.kt
@@ -4,6 +4,7 @@ import com.xenomachina.argparser.default
 import java.io.File
 import java.io.FileNotFoundException
 import java.io.PrintWriter
+import kotlin.random.Random
 
 class ArgumentsParser(parser: ArgParser) {
     val input by parser.storing(
@@ -18,7 +19,7 @@ class ArgumentsParser(parser: ArgParser) {
         } catch (ex: java.lang.NumberFormatException) {
             throw InvalidArgumentException("Input file contains lines that can't be converted to integer")
         }
-    }
+    }.default { ArrayList<Int>() }
 
     val output by parser.storing(
         "-o", "--output",
@@ -32,7 +33,7 @@ class ArgumentsParser(parser: ArgParser) {
     }.default(PrintWriter(System.out))
 
     val totalPages by parser.storing(
-        "-t", "--total-pages",
+        "-p", "--total-pages",
         help = "Total pages in memory"
     ) {
         try {
@@ -52,5 +53,18 @@ class ArgumentsParser(parser: ArgParser) {
             throw InvalidArgumentException("$this is not an integer")
         }
     }
+
+    val tests by parser.storing(
+        "-t", "--test-size",
+        help = "Amount of lines to generate(used instead of input file)"
+    ) {
+        try {
+             val tests = List(this.toInt()) { Random.nextInt(0, totalPages) }
+            println(tests)
+            tests
+        } catch (ex: NumberFormatException) {
+            throw InvalidArgumentException("$this is not an integer")
+        }
+    }.default { ArrayList<Int>() }
 
 }
--- src/VirtualMemory.kt
+++ src/VirtualMemory.kt
@@ -1,15 +1,22 @@
 @file: JvmName("VirtualMemory")
 
 import com.xenomachina.argparser.ArgParser
+import com.xenomachina.argparser.InvalidArgumentException
 
 fun main(args: Array<String>) {
     ArgParser(args).parseInto(::ArgumentsParser).run {
+        if (tests.isEmpty() && input.isEmpty()) {
+            throw InvalidArgumentException("No input values present. Please refer to --help")
+        }
+
+        val pagesQueue = if (tests.isEmpty()) input else tests
+
         val fifoMemory = FIFOMemory(RAMPages, totalPages)
         val lruMemory = LRUMemory(RAMPages, totalPages)
         val optMemory = OPTMemory(RAMPages, totalPages)
-        optMemory.constructFutureQueries(input)
+        optMemory.constructFutureQueries(pagesQueue)
 
-        input.forEach {
+        pagesQueue.forEach {
             output.println("${fifoMemory.getPage(it)} ${lruMemory.getPage(it)} ${optMemory.getPage(it)}")
         }
         output.flush()
